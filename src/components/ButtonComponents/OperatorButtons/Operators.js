26e28bab28b41b2556cdad0e12f4f3aedaaf8880
ampersand <ampersand@nhs-MacBook-Pro.local>
2019-08-13 14:10:48 -0700

    step 2 - imports/exports, usestate

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import OperatorButton from './OperatorButton'
 
 //Import your array data to from the provided data file
+import { operators } from '../../../data'
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorsState, setOperatorsState] = useState(operators);
+  console.log('operators', { operatorsState });
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
@@ -14,3 +19,5 @@ const Operators = () => {
     </div>
   );
 };
+
+export default Operators;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import SpecialButton from './SpecialButton'
 
 //Import your array data to from the provided data file
+import { specials } from '../../../data'
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialsState, setSpecialsState] = useState(specials);
+  console.log('specials', { specialsState });
 
   return (
     <div>
@@ -15,3 +19,5 @@ const Specials = () => {
     </div>
   );
 };
+
+export default Specials;
