43247e1f85521365b9c3c2732ce3bf3a5c300013
ampersand <ampersand@nhs-MacBook-Pro.local>
2019-08-13 17:49:47 -0700

    logo

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="container">
-      <Logo className='logo'/>
+      <Logo/>
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display />
