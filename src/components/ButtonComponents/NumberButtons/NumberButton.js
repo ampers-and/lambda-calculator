c09f160061c064c0a99e0c524b5f2e0f8fdae8c2
ampersand <ampersand@nhs-MacBook-Pro.local>
2019-08-13 17:51:40 -0700

    minor fixes

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,8 +3,6 @@ import React from "react";
 
 const NumberButton = (props) => {
   
-  // const number = props;
-  console.log('numButton', props)
   return (
     <button className='numButton'>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
